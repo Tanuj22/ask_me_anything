5839945477013456aa561fc2e5cbe7a0505b2dff
Tanuj22 <agarwal.5@iitj.ac.in>
2019-07-01 20:12:48 +0530

    added settings for registration and login

--- ask_me_anything/ask_me_anything/settings.py
+++ ask_me_anything/ask_me_anything/settings.py
@@ -37,8 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'rest_framework',
     'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'rest_auth',
+    'rest_auth.registration',
+    'crispy_forms',
     'users'
 ]
 
@@ -116,8 +123,26 @@ USE_L10N = True
 
 USE_TZ = True
 
+# login settings
+
+LOGIN_URL = "accounts/login"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# custom user model
+AUTH_USER_MODEL = "users.CustomUser"
+
+# crispy forms
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+# django.contrib.sites
+SITE_ID = 1
+
+# django-allauth
+ACCOUNT_EMAIL_VERIFICATION = "none"
+ACCOUNT_EMAIL_REQUIRED = (True)
