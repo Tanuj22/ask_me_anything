5c9eb7e542209c3ebd493829381fa9d27b62a19a
Tanuj22 <agarwal.5@iitj.ac.in>
2019-07-02 22:43:24 +0530

    added questions app and created its serializers

---
+++ ask_me_anything/questions/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.conf import settings
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.text import slugify
+import random
+import string
+
+
+class Question(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.CharField(max_length=240)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name="questions")
+
+    def __str__(self):
+        return self.content
+
+
+class Answer(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+    question = models.ForeignKey(Question,
+                                 on_delete=models.CASCADE,
+                                 related_name="answers")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+    voters = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                    related_name="votes")
+
+    def __str__(self):
+        return self.author.username
+
+
+ALPHANUMERIC_CHARS = string.ascii_lowercase + string.digits
+STRING_LENGTH = 6
+
+
+def generate_random_string(chars=ALPHANUMERIC_CHARS, length=STRING_LENGTH):
+    return "".join(random.choice(chars) for _ in range(length))
+
+
+@receiver(pre_save, sender=Question)
+def add_slug_to_question(sender, instance, *args, **kwargs):
+    if instance and not instance.slug:
+        slug = slugify(instance.content)
+        random_string = generate_random_string()
+        instance.slug = slug + "-" + random_string
