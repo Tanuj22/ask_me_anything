5e915f14d46381213e5d83e76d4c6ea5b8851f54
Tanuj22 <agarwal.5@iitj.ac.in>
2019-07-04 00:29:00 +0530

    added bootstrap and created navbar component

--- ask_me_anything/frontend/src/views/Home.vue
+++ ask_me_anything/frontend/src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+      <button class="btn btn-primary">Hello</button>
+    </div>
   </div>
 </template>
 
