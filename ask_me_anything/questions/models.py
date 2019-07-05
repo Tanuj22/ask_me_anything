5f2f4e2bcac0e154419597b69f125bda65da92d1
Tanuj22 <agarwal.5@iitj.ac.in>
2019-07-05 12:00:00 +0530

    added signals in different files

--- ask_me_anything/questions/models.py
+++ ask_me_anything/questions/models.py
@@ -1,17 +1,12 @@
 from django.db import models
 from django.conf import settings
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from django.utils.text import slugify
-import random
-import string
 
 
 class Question(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.CharField(max_length=240)
-    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL,
                                on_delete=models.CASCADE,
                                related_name="questions")
@@ -34,19 +29,3 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.author.username
-
-
-ALPHANUMERIC_CHARS = string.ascii_lowercase + string.digits
-STRING_LENGTH = 6
-
-
-def generate_random_string(chars=ALPHANUMERIC_CHARS, length=STRING_LENGTH):
-    return "".join(random.choice(chars) for _ in range(length))
-
-
-@receiver(pre_save, sender=Question)
-def add_slug_to_question(sender, instance, *args, **kwargs):
-    if instance and not instance.slug:
-        slug = slugify(instance.content)
-        random_string = generate_random_string()
-        instance.slug = slug + "-" + random_string
